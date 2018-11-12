414c484b1c6fd8e440036fa9b6137b880a2ceeb2
Julian <juliandisterheft@gmail.com>
2018-11-12 14:13:31 -0500

    Stuff

---
+++ Source/LeapTest2/LeapTest2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LeapTest2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LeapTest2, "LeapTest2" );
